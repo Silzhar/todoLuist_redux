28576b278aba26e1c1d2767986c0e1e8fd06df43
Silzhar <luis.ruizfer@gmail.com>
2020-11-26 20:26:21 +0100

    Add works

--- todoluist/src/components/Mainpage/Mainpage.jsx
+++ todoluist/src/components/Mainpage/Mainpage.jsx
@@ -4,7 +4,7 @@ import './Mainpage.scss'
 export function Mainpage() {
     return (
         <div>
-            <h1>TodoLuist</h1>
+            <h1 className='nameApp'>TodoLuist</h1>
         </div>
     )
 }
