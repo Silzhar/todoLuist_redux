127ace417cf492a2dc22c17b787b24a23bdf8869
Silzhar <luis.ruizfer@gmail.com>
2020-11-27 13:43:36 +0100

    Name App:color.

--- todoluist/src/components/Mainpage/Mainpage.jsx
+++ todoluist/src/components/Mainpage/Mainpage.jsx
@@ -4,7 +4,11 @@ import './Mainpage.scss'
 export function Mainpage() {
     return (
         <div>
-            <h1 className='nameApp'>TodoLuist</h1>
+            <h1 className='nameApp'>
+                <p className='nameApp__todo'>Todo</p>
+                <p className='nameApp__luis'>Luis</p>
+                <p className='nameApp__todo'>t</p>
+            </h1>
         </div>
     )
 }
