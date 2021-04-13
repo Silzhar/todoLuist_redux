a11e4b0cd4a934fec31b4dd82c29cec71ad3d134
Silzhar <luis.ruizfer@gmail.com>
2021-04-13 12:24:29 +0200

    Reorganize task

--- src/components/UrgentTask/UrgentTask.jsx
+++ src/components/UrgentTask/UrgentTask.jsx
@@ -1,48 +1,53 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { Card } from '../Card'
+import { Card } from '../Card';
 import {
-    // Selectors.
-    selectCreateCard,
-    selectDrawCreateCard
-    // Actions.
-} from '../../features/cardSlice'
+  // Selectors.
+  selectCreateCard,
+  selectDrawCreateCard,
+  // Actions.
+} from '../../features/cardSlice';
 
 import {
-    // Selectors.
-    // selectCreateCardurgent,
-    // Actions.
-    valuesUrgentTask
-    } from '../../features/urgentTaskSlice'
+  // Selectors.
+  // selectCreateCardurgent,
+  // Actions.
+  valuesUrgentTask,
+} from '../../features/urgentTaskSlice';
 
-import './UrgentTask.scss'
+import './UrgentTask.scss';
 
 export function UrgentTask() {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const createCard = useSelector(selectCreateCard)
-    const drawCreateCard = useSelector(selectDrawCreateCard)
+  const createCard = useSelector(selectCreateCard);
+  const drawCreateCard = useSelector(selectDrawCreateCard);
 
-    function handleSubmit() {
-        dispatch(valuesUrgentTask(createCard))
-    }
-    
-    return (
-        <div>
-            <h1>Tareas urgentes</h1>
-            <Card />
-            <form onSubmit={handleSubmit} className="urgentTaskInput">
+  function handleSubmit() {
+    dispatch(valuesUrgentTask(createCard));
+  }
 
-            </form>
-            {!drawCreateCard  ? (
-                    <div className="task">
-                      <p>{createCard}</p>
-                      <p>{createCard[0]} -- {createCard[1]}</p>
-                      <p>{createCard[2]} -- {createCard[3]}</p>
-                    </div>
-                ): null}
+  return (
+    <div>
+      <h1>Tareas urgentes</h1>
+      <Card />
+      <form onSubmit={handleSubmit} className="urgentTaskInput"></form>
+      {!drawCreateCard ? (
+        <div className="task">
+          {/* <p>{createCard}</p> */}
+          <div>
+            <p className="task__title">{createCard[0]}</p>
+            <p className="task__description">{createCard[1]}</p>
+          </div>
+          <div>
+            <p className="task__title">{createCard[2]}</p>
+            <p className="task__description">{createCard[3]}</p>
+          </div>
+
+          {/* <p>{createCard[2]} -- {createCard[3]}</p> */}
         </div>
-        
-    )
+      ) : null}
+    </div>
+  );
 }
